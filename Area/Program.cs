54c1aef7d24fdae5bcfa8f36713e5f60f5ffd339
Clarynaa <Clary.v.green@gmail.com>
2018-07-26 22:53:02 -0500

    Added Area and HelloWorld

---
+++ Area/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a radius: ");
+            float r = float.Parse(Console.ReadLine());
+            if (r < 0)
+            {
+                Console.WriteLine("Error!  Number negative!");
+                Console.ReadLine();
+            }
+            else
+            {
+                var area = 3.14 * r * r;
+                Console.WriteLine("The area of a circle with radius " + r + " is: " + Math.Round(area, 3));
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name + " What is the height of your rectangle?");
+            int height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What about the width?");
+            int width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("That rectangle has an area of " + (height * width) + "\nWhat would you like to search the first sentence of Alice in Wonderland for?");
+            string alice = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversation?'";
+            string searchTerm = Console.ReadLine().ToUpper();
+            bool search = alice.ToUpper().Contains(searchTerm);
+            Console.WriteLine(search);
+            Console.ReadLine();
+        }
+    }
+}
