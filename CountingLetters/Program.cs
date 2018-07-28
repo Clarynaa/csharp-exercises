983546ee44cca17cb0012166a7cfc9d1e24ab8ce
Clarynaa <Clary.V.Green@gmail.com>
2018-07-28 15:57:00 -0500

    Finished CountingLetters

---
+++ CountingLetters/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountingLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string toCompare = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+            string toCompareLower = toCompare.ToLower();
+            var letterCount = new Dictionary<char, int>();
+            for (int i = 0;  i < toCompare.Length; i++)
+            {
+                if (char.IsLetter(toCompareLower[i]))
+                {
+                    if (!letterCount.ContainsKey(toCompareLower[i]))
+                    {
+                        letterCount.Add(toCompareLower[i], 1);
+                    }
+                    else
+                    {
+                        letterCount[toCompareLower[i]]++;
+                    }
+                }
+
+                else if (char.IsNumber(toCompareLower[i]))
+                {
+                    if (!letterCount.ContainsKey(toCompareLower[i]))
+                    {
+                        letterCount.Add(toCompareLower[i], 1);
+                    }
+                    else
+                    {
+                        letterCount[toCompareLower[i]]++;
+                    }
+                }
+            }
+
+            foreach (var pair in letterCount)
+            {
+                Console.Out.WriteLine(pair.Key + ": " + pair.Value);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
