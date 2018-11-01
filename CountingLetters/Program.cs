3f840d6b496f31c3351b1d4f4b28f3368cace601
Clarynaa <32001815+Clarynaa@users.noreply.github.com>
2018-11-01 13:23:05 -0500

    Further expanded CountingLetters to use user input until input is blank

--- CountingLetters/Program.cs
+++ CountingLetters/Program.cs
@@ -9,9 +9,21 @@ namespace CountingLetters
         static void Main(string[] args)
         {
             string toCompare = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+
+            while (toCompare != "")
+            {
+                Counting(toCompare);
+                Console.WriteLine("Please enter a string or leave blank to exit: ");
+                toCompare = Console.ReadLine();
+
+            }
+        }
+
+        static void Counting(string toCompare)
+        {
             string toCompareLower = toCompare.ToLower();
             var letterCount = new Dictionary<char, int>();
-            for (int i = 0;  i < toCompare.Length; i++)
+            for (int i = 0; i < toCompare.Length; i++)
             {
                 if (char.IsLetter(toCompareLower[i]))
                 {
@@ -38,12 +50,14 @@ namespace CountingLetters
                 }
             }
 
+            Console.WriteLine("Unsorted: ");
+
             foreach (var pair in letterCount)
             {
                 Console.Out.WriteLine(pair.Key + ": " + pair.Value);
             }
 
-            Console.Out.WriteLine("-----------------------------------------");
+            Console.Out.WriteLine("-----------------------------------------\nSorted:");
             List<char> letters = letterCount.Keys.ToList();
             letters.Sort();
 
@@ -51,10 +65,6 @@ namespace CountingLetters
             {
                 Console.Out.WriteLine(letter + ": " + letterCount[letter]);
             }
-
-
-            Console.ReadLine();
-
         }
     }
 }
